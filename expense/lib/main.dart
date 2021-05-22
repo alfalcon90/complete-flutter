3d9c7327e42295226cfdffcdd84a57822567c217
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2021-05-21 17:12:45 -0700

    expense tracker init

---
+++ expense/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Container(
+              width: double.infinity,
+              child: Text('CHART'),
+            ),
+            elevation: 5,
+          ),
+          Card(
+            child: Text('List of TX'),
+          )
+        ],
+      ),
+    );
+  }
+}
